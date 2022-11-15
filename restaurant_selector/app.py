2d39654a9667cd61ea3bb5f45f20f3053e2db253
Harika Duyu <harika.duyu@gmail.com>
2022-11-15 15:18:56 +0100

    parse OSM data as objects

--- restaurant_selector/app.py
+++ restaurant_selector/app.py
@@ -1,5 +1,5 @@
 import logging
-from random import randint
+from random import choice
 from enum import Enum
 from typing import List
 import json
@@ -26,51 +26,47 @@ templates = Jinja2Templates(directory="templates")
 app = FastAPI()
 
 
-def set_up_database(db : SessionLocal = None):
-    f = open("restaurant_list.json",)
-    restaurants = json.load(f)
-    for restaurant in restaurants:
-        restaurant = restaurant["properties"]
-        if "name" not in restaurant:
-            continue
-        name = restaurant["name"]
-        logger.debug(f"NAME = {name}")
-        logger.debug(restaurant)
-        email = restaurant["email"]
-
-        # new_restaurant = models.Restaurant(
-        #                     name=restaurant["name"],
-        #                     cuisine = restaurant["cuisine"]
-        #                     take_away = restaurant.get("take_away", None)
-        #                     email = restaurant.get("email", None)
-        #                     indoor_seating = 
-        #                     level = 
-        #                     opening_hours = 
-        #                     outdoor_seating = 
-        #                     payment_mastercard = 
-        #                     payment_visa = 
-        #                     phone = 
-        #                     website = 
-        #                     wheelchair = 
-        #                     smoking = 
-        #                     diet_vegan = 
-        #                     diet_vegetarian = 
-        # db.add(new_restaurant)
-    # db.commit()
-    
-
-set_up_database()
-
 def get_db():
     db = SessionLocal()
     try:
-        restaurants = db.query(models.Restaurant).all()
-        if not restaurants:
-            set_up_database(db)
+        set_up_database(db)
         yield db
     finally:
         db.close()
 
+
+
+
+def set_up_database(db : Session = Depends(get_db)):
+    restaurants = db.query(models.Restaurant).first()
+    if not restaurants:
+        f = open("restaurant_list.json",)
+        restaurants = json.load(f)
+        for restaurant in restaurants:
+            restaurant = restaurant["properties"]
+            if "name" not in restaurant:
+                continue
+
+            new_restaurant = models.Restaurant(
+                                name=restaurant["name"],
+                                cuisine = restaurant.get("cuisine", None),
+                                take_away = restaurant.get("take_away", None),
+                                email = restaurant.get("email", None),
+                                indoor_seating = restaurant.get("indoor_seating" , None),
+                                level = restaurant.get("level", None),
+                                opening_hours = restaurant.get("opening_hours", None),
+                                outdoor_seating = restaurant.get("outdoor_seating", None),
+                                payment_mastercard = restaurant.get("payment:mastercard", None),
+                                payment_visa = restaurant.get("payment:visa", None),
+                                phone = restaurant.get("phone", None),
+                                website = restaurant.get("website", None),
+                                wheelchair = restaurant.get("wheelchair", None),
+                                smoking = restaurant.get("smoking", None),
+                                diet_vegan = restaurant.get("diet:vegan", None),
+                                diet_vegetarian = restaurant.get("diet:vegetarian", None))
+            db.add(new_restaurant)
+        db.commit()
+    
 
 
 # Dependency
@@ -84,28 +80,55 @@ class RestaurantTags(str, Enum):
     korean = "Korean"
     mexican = "Mexican"
 
+
+
 @app.get("/")
 async def home(req: Request, db: Session = Depends(get_db)):
-    f = open("restaurant_list.json",)
-    restaurants = json.load(f)
-
+    restaurants = db.query(models.Restaurant).all()
     return templates.TemplateResponse("base.html",
                                       {"request": req, "restaurant_list": restaurants})
 
 
 
 @app.post("/add")
-def add(req: Request,
-        title: str = Form(...),
-        emoji: str = Form(...),
-        tags: List[str] = Form(...),
-        # tags: List[str] = Form(...),
-        db: Session = Depends(get_db)
+def add(
+    req: Request,
+    name: str = Form(...),
+    cuisine: str = Form(...),
+    take_away: str = Form(...),
+    # email: str = Form(...),
+    indoor_seating: str = Form(...),
+    # level: str = Form(...),
+    opening_hours: str = Form(...),
+    # outdoor_seating: str = Form(...),
+    # payment_mastercard: str = Form(...),
+    # payment_visa: str = Form(...),
+    # phone: str = Form(...),
+    # website: str = Form(...),
+    # wheelchair: str = Form(...),
+    # smoking: str = Form(...),
+    # diet_vegan: str = Form(...),
+    # diet_vegetarian: str = Form(...),
+    db: Session = Depends(get_db)
 ):
-    logger.info(f"req {req}")
-    logger.info(f"tags {tags}")
-    logger.info(f"emoji {emoji}")
-    new_restaurant = models.Restaurant(title=title, emoji=emoji, tags=", ".join(tags))
+    new_restaurant =  models.Restaurant(
+                                name = name,
+                                cuisine = cuisine,
+                                take_away = take_away,
+                                # email = email,
+                                indoor_seating = indoor_seating,
+                                # level = level,
+                                opening_hours = opening_hours,
+                                # outdoor_seating = outdoor_seating,
+                                # payment_mastercard = payment_mastercard,
+                                # payment_visa = payment_visa,
+                                # phone = phone,
+                                # website = website,
+                                # wheelchair = wheelchair,
+                                # smoking = smoking,
+                                # diet_vegan = diet_vegan,
+                                # diet_vegetarian = diet_vegetarian
+                            )
     db.add(new_restaurant)
     db.commit()
     url = app.url_path_for("home")
@@ -133,18 +156,11 @@ def delete(req: Request, restaurant_id: int, db: Session = Depends(get_db)):
 
 @app.get("/random_restaurant")
 def get_random_restaurant(req: Request, db: Session = Depends(get_db)):
-    f = open("restaurant_list.json",)
-    restaurants = json.load(f)
-    if restaurants:
-        random_index = randint(0,len(restaurants)-1)
-        selected_restaurant =  restaurants[random_index]["properties"]
+    selected_restaurant = choice(db.query(models.Restaurant).all())
     return selected_restaurant
 
 
 
 @app.get("/all_restaurants")
 def get_random_restaurant(req: Request, db: Session = Depends(get_db)):
-    f = open("restaurant_list.json",)
-    restaurants = json.load(f)
-
-    return restaurants
+    return db.query(models.Restaurant).all()
